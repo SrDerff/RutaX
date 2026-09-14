bb41bbd9fcf392a3c21ae0784759bdd155cd889b
Derff <srderfflinqer@gmail.com>
2026-09-13 23:59:04 -0500

    map entries

--- src/main/java/com/rutax/Main.java
+++ src/main/java/com/rutax/Main.java
@@ -3,6 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        
+        Map<String,String> mp = new HashMap<>();
+
+        mp.put("name", "alex");
+        mp.put("nick", "derff");
+
+        for(Map.Entry<String,String>p : mp.entrySet()){
+            System.out.println(p.getKey());
+            System.out.println(p.getValue());
+        }
     }
 }
