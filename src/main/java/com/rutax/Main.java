3aa7e97b66144b779f289c7cfa369938ee383932
Derff <srderfflinqer@gmail.com>
2026-09-13 23:44:45 -0500

    Main

--- src/main/java/com/rutax/Main.java
+++ src/main/java/com/rutax/Main.java
@@ -3,13 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Map<String, String> mp = new HashMap<>();
-        mp.put("name","alex");
-        mp.put("age","18");
-        mp.put("email","sq@gmail.com");
-
-        for(Map.Entry<String, String> m : mp.entrySet()){
-            System.out.print(m.getKey() + " " + m.getValue() + "\n");
-        }
+        
     }
 }
