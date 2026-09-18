b46b541f1b363fb28443e456a27cfe7790caa355
Derff <srderfflinqer@gmail.com>
2026-09-18 01:30:28 -0500

    feat: auth service

--- src/main/java/com/rutax/auth/Auth.java
+++ src/main/java/com/rutax/auth/Auth.java
@@ -1,4 +1,56 @@
 package com.rutax.auth;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class Auth {
+    final static String USERS_PATH=".\\.\\resources\\users\\users_list.txt";
+
+    public static boolean login(String username, String password){
+        Path path = Path.of(USERS_PATH);
+        try{
+            List<String> lines = Files.readAllLines(path);
+            for(String line: lines){
+                String[] data = line.split(":");
+
+                String storedUsername=data[0];
+                String storedPassword=data[1];
+
+                if(storedUsername.equals(username) && storedPassword.equals(password)){
+                    return true;
+                }
+            }
+        }catch (IOException excep){
+            excep.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean register(String username, String password, String name){
+        Path path = Path.of(USERS_PATH);
+        try{
+            List<String> lines = Files.readAllLines(path);
+            for(String line: lines){
+                String[] data = line.split(":");
+
+                String storedUsername=data[0];
+
+                if(storedUsername.equals(username)){
+                    return false;
+                }
+            }
+            Files.writeString(
+                    path,
+                    username+":"+password+":"+name+System.lineSeparator(),
+                    StandardOpenOption.APPEND
+            );
+            return true;
+        }catch (IOException excep){
+            excep.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/com/rutax/auth/User.java
+++ src/main/java/com/rutax/auth/User.java
@@ -1,4 +1,34 @@
 package com.rutax.auth;
 
 public class User {
+    private final String username;
+    private String password;
+    private String name;
+
+    public User(String _username, String _password, String _name){
+        this.username=_username;
+        this.password=_password;
+        this.name=_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
